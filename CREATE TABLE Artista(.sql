0796371d29d32ccca714c864f80551aa59df240d
Mateo Jaimes <m.jaimesr2@uniandes.edu.co>
2025-09-18 12:45:37 -0500

    albumes mismo func, sin FK

--- CREATE TABLE Artista(.sql
+++ CREATE TABLE Artista(.sql
@@ -1,3 +1,5 @@
+DROP TABLE Artista
+
 CREATE TABLE Artista(
     id_artista NUMBER PRIMARY KEY,
     nombre VARCHAR2(120) NOT NULL  
@@ -7,7 +9,7 @@ CREATE TABLE Artista(
 CREATE SEQUENCE seqIdArtista
 START WITH 1 
 INCREMENT BY 1
-NOCACHE;
+CACHE 1 ;
 
 INSERT INTO Artista (id_artista, nombre) VALUES (seqIdArtista.NEXTVAL, 'Shakira');
 INSERT INTO Artista (id_artista, nombre) VALUES (seqIdArtista.NEXTVAL, 'Juanes');
@@ -17,3 +19,24 @@ INSERT INTO Artista (id_artista, nombre) VALUES (seqIdArtista.NEXTVAL, 'Maluma')
 
 SELECT * FROM Artista;
 
+
+DROP TABLE Album
+
+CREATE TABLE Album(
+    id_album NUMBER PRIMARY KEY,
+    titulo VARCHAR2(160) NOT NULL  
+
+);
+
+CREATE SEQUENCE seqIdAlbum
+START WITH 1 
+INCREMENT BY 1
+NOCACHE;
+
+INSERT INTO Album (id_album, titulo) VALUES (seqIdAlbum.NEXTVAL, 'Pies Descalzos');
+INSERT INTO Album (id_album, titulo) VALUES (seqIdAlbum.NEXTVAL, 'Un Día Normal');
+INSERT INTO Album (id_album, titulo) VALUES (seqIdAlbum.NEXTVAL, 'La Tierra del Olvido');
+INSERT INTO Album (id_album, titulo) VALUES (seqIdAlbum.NEXTVAL, 'Energía');
+INSERT INTO Album (id_album, titulo) VALUES (seqIdAlbum.NEXTVAL, 'Fijación Oral Vol. 1');
+
+SELECT * FROM Album;
