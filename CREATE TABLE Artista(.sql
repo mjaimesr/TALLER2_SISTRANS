d20661b8ffad91d7cdd187420cb87c5d5699516b
mjaimesr <m.jaimesr2@uniandes.edu.co>
2025-09-18 11:38:51 -0500

    Add files via upload

---
+++ CREATE TABLE Artista(.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Artista(
+    id_artista NUMBER PRIMARY KEY,
+    nombre VARCHAR2(120) NOT NULL  
+);
+
+INSERT INTO Artista (id_artista, nombre) VALUES (1, 'Shakira');
+INSERT INTO Artista (id_artista, nombre) VALUES (2, 'Juanes');
+INSERT INTO Artista (id_artista, nombre) VALUES (3, 'Carlos Vives');
+INSERT INTO Artista (id_artista, nombre) VALUES (4, 'J Balvin');
