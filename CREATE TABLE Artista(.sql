12b9952c8ad59e204f80d74c56f813898589059b
Daniel Andres Gomez Avila <da.gomeza1@uniandes.edu.co>
2025-09-18 11:41:17 -0500

    correccion

--- CREATE TABLE Artista(.sql
+++ CREATE TABLE Artista(.sql
@@ -7,5 +7,3 @@ INSERT INTO Artista (id_artista, nombre) VALUES (1, 'Shakira');
 INSERT INTO Artista (id_artista, nombre) VALUES (2, 'Juanes');
 INSERT INTO Artista (id_artista, nombre) VALUES (3, 'Carlos Vives');
 INSERT INTO Artista (id_artista, nombre) VALUES (4, 'J Balvin');
-
-SXSX
